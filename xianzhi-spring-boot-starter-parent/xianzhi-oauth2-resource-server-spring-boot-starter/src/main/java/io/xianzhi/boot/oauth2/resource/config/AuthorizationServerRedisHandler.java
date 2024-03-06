7135b0473c80a6bbe07980ff33437532c7841729
Ethan Wang <max@xianzhi.io>
2024-03-06 21:38:38 +0800

    init project

--- xianzhi-spring-boot-starter-parent/xianzhi-oauth2-resource-server-spring-boot-starter/src/main/java/io/xianzhi/boot/oauth2/resource/config/AuthorizationServerRedisHandler.java
+++ xianzhi-spring-boot-starter-parent/xianzhi-oauth2-resource-server-spring-boot-starter/src/main/java/io/xianzhi/boot/oauth2/resource/config/AuthorizationServerRedisHandler.java
@@ -1,6 +1,21 @@
+/*
+ * Copyright (c) 2023-2024  XianZhi Group All Rights
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.xianzhi.boot.oauth2.resource.config;
 
-import cn.hutool.json.ObjectMapper;
 import com.alibaba.fastjson2.support.spring6.data.redis.FastJsonRedisSerializer;
 import lombok.Getter;
 import lombok.Setter;
@@ -12,7 +27,6 @@ import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettucePoolingClientConfiguration;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.util.Assert;
